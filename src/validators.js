ef0ab8687252f3622517783d523eaad6b8a39222
Martin Virando <mvirando@cisco.com>
2018-08-08 17:51:17 +0100

    Started setup for the npm package

--- src/validators.js
+++ src/validators.js
@@ -10,3 +10,4 @@ export const emailCheck = err => value => {
   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(String(value).toLowerCase()) ? null : err;
 };
+
--- src/withForm.jsx
+++ src/withForm.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { _checkInputForErrors, _getValues } from './utilities';
+import compose from 'recompose/compose';
 
 const propTypes = {
   onSubmit: PropTypes.func.isRequired,
@@ -15,6 +15,74 @@ const defaultProps = {
   onChange: () => {},
   className: undefined,
   resetFormOnSubmit: false,
+};
+
+/**
+* Get the values from the inputs.
+* @param {object} inputs - The inputs form the form.
+*/
+export const _getValues = inputs =>
+  Object.entries(inputs).reduce((obj, [key, { value }]) => ({
+    ...obj,
+    [key]: value,
+  }), {});
+
+/**
+* Validate the input and return the input with any errors
+* @param {object} input - The input field to be validated.
+*/
+export const _validate = (input = {}) => {
+  const errors = input.validation
+    .map(fn => fn(input.value))
+    .filter(v => !!v);
+
+  return {
+    ...input,
+    errors: [
+      ...input.errors,
+      ...errors,
+    ],
+  };
+};
+
+/**
+* Validate the matchers on the input. This is good if values need to match.
+* @param {object} input - The input field to be validated.
+* @param {object} inputs - The inputs from the form state.
+*/
+export const _validateMatchers = (input, inputs) => {
+  if (!input.matchers || !input.matchers.length) {
+    return input;
+  }
+
+  const errors = input.matchers
+    .map(fn => fn(input.value, inputs))
+    .filter(v => !!v);
+
+  return {
+    ...input,
+    errors,
+  };
+};
+
+/**
+* Check a single input for errors.
+*/
+export const _checkInputForErrors = (input, inputs, touched = false) => {
+  if (!input.touched && !touched) {
+    return input;
+  }
+
+  const newInput = {
+    ...input,
+    errors: [],
+  };
+
+  // Now check the input for errors.
+  return compose(
+    _validate,
+    _validateMatchers,
+  )(newInput, inputs);
 };
 
 const formHoc = config => (WrappedComponent) => {
